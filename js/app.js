8e2df363b87494c9e84fe65dfa29daec6ff9fca4
ilkhomsafarov <ilkhoms96@gmai.com>
2022-09-17 19:03:19 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,181 @@
+//1-masala
+// let a = prompt("son kirit")
+
+// if(a>0){
+//     console.log(true)
+// }
+// else if(a==0){
+//     console.log("son natogri kiritilgan")
+// }
+// else{
+//     console.log(false)
+// }
+
+//2-masala
+// let a = prompt("son kirit")
+
+// if (a%2!=0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//3-masala
+// let a = prompt("son kirit")
+
+// if (a%2==0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+
+//4-masala
+// let a = prompt("son kirit")
+// let b = prompt("son kirit")
+
+// if(a>0 && b>0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//5-masala
+// let a = prompt("son kirit")
+// let b = prompt("son kirit")
+// let c = prompt("son kirit")
+
+// if(a>b && b>c){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//6-masala
+// let a = prompt("son kirit")
+// let b = prompt("son kirit")
+// let c = prompt("son kirit")
+
+// if(a<b && b<c){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//7-masala
+// let a = prompt("sonkirit")
+// let b = prompt("sonkirit")
+
+// if(a%2==0 && b%2==0){
+//     console.log("juft")
+// }
+// else{
+//     console.log("toq")
+// }
+
+//8-masala
+// let a = prompt("a son kir It")
+// let b = prompt("b son kir It")
+// let c = prompt("c son kir It")
+// let d = prompt("d son kir It")
+
+// if(a>0 && b>0 && c>0 && d>0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//9-masala
+// let a = prompt("a son kir It")
+// let b = prompt("b son kir It")
+// let c = prompt("c son kir It")
+
+// if(a>0 && b>0 || b>0 && c>0|| a>0 && c>0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//10-masala
+// let a=prompt("1-7 gacha son kirit")
+// const week = ["dushanba", "seshanbi", "chorshanbi", "payshanbi", "juma", "shanbi", "yakshanbi"]
+
+// if(a==1){
+//     console.log(week[0])
+// }
+// else if(a==2){
+//     console.log(week[1])
+// }
+// else if(a==3){
+//     console.log(week[2])
+// }
+// else if(a==4){
+//     console.log(week[3])
+// }
+// else if(a==5){
+//     console.log(week[4])
+// }
+// else if(a==6){
+//     console.log(week[5])
+// }
+// else if(a==7){
+//     console.log(week[6])
+// }
+// else{
+//     console.log("Error 404")
+// }
+
+//11-masala
+// let a=prompt("kod kirit")
+// const kod =["ums", "ucell", "beeline", "perfectum", "uzmobile"]
+
+// if(a==97 || a==88 || a==77){
+//     console.log(kod[0])
+// }
+// else if(a==93 || a==94){
+//     console.log(kod[1])
+// }
+// else if(a==90 || a==91){
+//     console.log(kod[2])
+// }
+// else if(a==98){
+//     console.log(kod[3])
+// }
+// else if(a==99){
+//     console.log(kod[4])
+// }
+// else{
+//     console.log("bunday kod mavjud emas")
+// }
+
+//12-masala
+// let a=Number(prompt("son kirit"))
+
+// if(a>0){
+//     console.log(a+1)
+// }
+// else if(a<0){
+//     console.log(a-1)
+// }
+// else{
+//     console.log("0 dan kotta yoki kichik son kirit")
+// }
+
+//13-masala
+// let a=Number(prompt("son kirit"))
+
+// if(a>0){
+//     console.log(a+3)
+// }
+// else if(a<0){
+//     console.log(a-2)
+// }
+// else{
+//     console.log("0 dan kotta yoki kichik son kirit")
+// }
+
